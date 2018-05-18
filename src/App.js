e7b3a5a70c51a194bb0abafb6ee2b1c6e92c6dca
laurengranada <laurengranada@utexas.edu>
2018-05-17 23:21:11 -0500

    added react-bootstrap and added a few boostrap components to homepage

--- src/App.js
+++ src/App.js
@@ -6,15 +6,35 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+      <ul class="nav nav-tabs">
+		<li role="presentation" class="active"><a href="#">Home</a></li>
+		<li role="presentation"><a href="#">Saved Articles</a></li>
+		<li role="presentation" class="navbar-right"><a href="#">Scrape Articles Here </a></li>
+	  </ul>
         <header className="App-header">
           <h1 className="App-title">MongoScraper</h1>
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Edition</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-      </div>
+
+		    <div class="thumbnail">
+		      <div class="caption">
+		        <h3>Apple to sell 1st gen iPhones again</h3>
+		        <p>According to a survey done by the company, some customers are nostalgic of the features from 1st gen iPhones.</p>
+		        <p><a href="#" class="btn btn-primary" role="button">Save Article</a></p>
+		      </div>
+		    </div>
+		    <div class="thumbnail">
+		      <div class="caption">
+		        <h3>Mark Zuckerberg to appear before European Parliamennt</h3>
+		        <p>Facebook CEO Mark Zuckerberg has agreed to come to the European Parliament, according to the parliament's top official.</p>
+		        <p><a href="#" class="btn btn-primary" role="button">Save Article</a></p>
+		      </div>
+		    </div>
+	  </div>
+
     );
   }
 }
